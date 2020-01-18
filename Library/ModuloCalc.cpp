77c63b8340f1f961005e21a5a57ec53f97343626
Fumiya Suto <fumiya.suto@gmail.com>
2020-01-18 18:36:08 +0900

    add perm function to ModuloCalc

--- Library/ModuloCalc.cpp
+++ Library/ModuloCalc.cpp
@@ -1,6 +1,6 @@
 // 剰余計算
 // verifyed
-// 
+// https://atcoder.jp/contests/keyence2019/tasks/keyence2019_f
 
 long long modPow(long long a, long long p){
     if(p == 0) return 1;
@@ -34,18 +34,21 @@ int moduloLog(int a, int target){
     return -1;
 }
 
-{
-	vector<long long> inv(200002);
-	vector<long long> fact(200002);
-	vector<long long> factInv(200002);
+    const int maxSize = 100001;
+	vector<long long> inv(maxSize);
+	vector<long long> fact(maxSize);
+	vector<long long> factInv(maxSize);
 	for(int i=0;i<2;i++) inv[i] = fact[i] = factInv[i] = 1;
-	for(int i=2;i<=inv.size()-1;i++){
+	for(int i=2;i<maxSize;i++){
 		inv[i] = inv[MOD % i] * (MOD - MOD / i) % MOD;
 		fact[i] = fact[i-1] * i % MOD;
 		factInv[i] = factInv[i-1] * inv[i] % MOD;
 	}
 	auto comb = [&](int n, int r){
-		if(n < r) return 0LL;
+		if(n < r || r < 0) return 0LL;
 		return fact[n] * factInv[n-r] % MOD * factInv[r] % MOD;
 	};
-}
+    auto perm = [&](int n, int r){
+        if(n < r || r < 0) return 0LL;
+        return fact[n] * factInv[n-r] % MOD;
+    };
