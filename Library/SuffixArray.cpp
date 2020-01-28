0897ee745a2dba2b26e238bb882b1b8cc60874a2
Fumiya Suto <fumiya.suto@gmail.com>
2020-01-28 20:27:09 +0900

    add SuffixArray

---
+++ Library/SuffixArray.cpp
@@ -0,0 +1,39 @@
+// 接尾辞配列 (Larsson-Sadakane法)
+// Verifyed
+// https://atcoder.jp/contests/ddcc2020-final/tasks/ddcc2020_final_c
+
+vector<int> getSuffixArray(const auto& str, int alphabetSize){
+	const int n = str.size();
+	vector<pair<int, int>> vp(n);
+	vector<int> rank(n), cnt(alphabetSize, 0);
+	for(int i=0;i<n;i++) cnt[str[i]]++;
+	for(int i=1;i<alphabetSize;i++) cnt[i] += cnt[i-1];
+	for(int i=0;i<n;i++){
+		rank[i] = vp[i].first = cnt[str[i]];
+		vp[i].second = i;
+	}
+	sort(vp.begin(), vp.end());
+	for(int h=1;h<n;h*=2){
+		auto cur = vp.begin();
+		while(cur != vp.end()){
+			auto end = cur;
+			int key = rank[cur->second];
+			while(end != vp.end() && rank[end->second] == key){
+				end->first = end->second + h < n ? rank[end->second + h] : -1;
+				end++;
+			}
+			int s = distance(vp.begin(), cur), e = distance(vp.begin(), end);
+			if(e - s > 1){
+				sort(cur, end);
+				rank[vp[e-1].second] = key;
+				for(int i=e-2;i>=s;i--){
+					rank[vp[i].second] = (vp[i].first == vp[i+1].first ? rank[vp[i+1].second] : key - (e-1-i));
+				}
+			}
+			cur = end;
+		}
+	}
+    vector<int> res(n);
+	for(int i=0;i<n;i++) res[i] = vp[i].second;
+    return res;
+}
