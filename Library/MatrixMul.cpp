c9b0e64e1f4cf0b7f31487adf7fe6d8a206acde3
Fumiya Suto <fumiya.suto@gmail.com>
2020-01-28 19:47:17 +0900

    add operator to Matrix class

--- Library/MatrixMul.cpp
+++ Library/MatrixMul.cpp
@@ -1,6 +1,7 @@
 // 行列累乗
 // verifyed
 // https://atcoder.jp/contests/abc009/tasks/abc009_4
+// https://atcoder.jp/contests/ddcc2020-final/tasks/ddcc2020_final_b
 
 template<typename T>
 class Matrix {
@@ -11,18 +12,21 @@ public:
         for(int i=0;i<N;i++) res.m[i][i] = 1;
         return res;
     }
+    vector<T>& operator [](int n) { return m[n]; }
+    const vector<T>& operator [](int n) const { return m[n]; }
+    int size() const { return m.size(); }
 public:
     vector<vector<T>> m;
 };
 
 template<typename T>
 Matrix<T> mul(const Matrix<T>& a, const Matrix<T>& b){
-    const int N = a.m.size();
+    const int N = a.size();
     Matrix<T> res(N);
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            res.m[i][j] = 0;
-            for(int k=0;k<N;k++) res.m[i][j] += a.m[i][k] * b.m[k][j];
+            res[i][j] = 0;
+            for(int k=0;k<N;k++) res[i][j] += a[i][k] * b[k][j];
         }
     }
     return res;
@@ -30,7 +34,7 @@ Matrix<T> mul(const Matrix<T>& a, const Matrix<T>& b){
 
 template<typename T>
 Matrix<T> pow(const Matrix<T>& a, int p){
-    const int N = a.m.size();
+    const int N = a.size();
     if(p == 0) return Matrix<T>::ident(N);
     auto res = pow(a, p/2);
     res = mul(res, res);
@@ -40,11 +44,11 @@ Matrix<T> pow(const Matrix<T>& a, int p){
 
 template<typename T>
 vector<T> vecMul(const Matrix<T>& a, const vector<T>& v){
-    const int N = a.m.size();
+    const int N = a.size();
     vector<T> res(N, 0);
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            res[i] += a.m[i][j] * v[j];
+            res[i] += a[i][j] * v[j];
         }
     }
     return res;
@@ -52,15 +56,16 @@ vector<T> vecMul(const Matrix<T>& a, const vector<T>& v){
 
 template<typename T>
 Matrix<T> modMul(const Matrix<T>& a, const Matrix<T>& b, int mod){
-    const int N = a.m.size();
+    const int N = a.size();
     Matrix<T> res(N);
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            res.m[i][j] = 0;
+            res[i][j] = 0;
             for(int k=0;k<N;k++){
-                res.m[i][j] += a.m[i][k] * b.m[k][j];
+                res[i][j] += a[i][k] * b[k][j];
+                res[i][j] %= mod;
             }
-                res.m[i][j] %= mod;
+            // res[i][j] %= mod; // 安全ならこっちでmodをとる
         }
     }
     return res;
@@ -68,7 +73,7 @@ Matrix<T> modMul(const Matrix<T>& a, const Matrix<T>& b, int mod){
 
 template<typename T>
 Matrix<T> modPow(const Matrix<T>& a, int p, int mod){
-    const int N = a.m.size();
+    const int N = a.size();
     if(p == 0) return Matrix<T>::ident(N);
     auto res = modPow(a, p/2, mod);
     res = modMul(res, res, mod);
@@ -78,11 +83,11 @@ Matrix<T> modPow(const Matrix<T>& a, int p, int mod){
 
 template<typename T>
 vector<T> vecModMul(const Matrix<T>& a, const vector<T>& v, int mod){
-    const int N = a.m.size();
+    const int N = a.size();
     vector<T> res(N, 0);
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            res[i] += a.m[i][j] * v[j];
+            res[i] += a[i][j] * v[j];
             res[i] %= mod;
         }
     }
