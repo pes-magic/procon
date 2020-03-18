d4b83e4206e325f70118c8a3bbd5968da865a972
Fumiya Suto <fumiya.suto@gmail.com>
2020-03-18 20:38:31 +0900

    add AGC040 C

---
+++ AtCoder/AGC040/C.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MOD = 998244353;
+
+long long modPow(long long a, long long p){
+    if(p == 0) return 1;
+    auto res = modPow(a, p/2);
+    res = (res*res)%MOD;
+    if(p%2) res = (res*a)%MOD;
+    return res;
+}
+
+long long calcInv(long long a){
+    return modPow(a, MOD-2);
+}
+
+int main(){
+    const int maxSize = 10000001;
+	vector<long long> inv(maxSize);
+	vector<long long> fact(maxSize);
+	vector<long long> factInv(maxSize);
+	for(int i=0;i<2;i++) inv[i] = fact[i] = factInv[i] = 1;
+	for(int i=2;i<maxSize;i++){
+		inv[i] = inv[MOD % i] * (MOD - MOD / i) % MOD;
+		fact[i] = fact[i-1] * i % MOD;
+		factInv[i] = factInv[i-1] * inv[i] % MOD;
+	}
+	auto comb = [&](int n, int r){
+		if(n < r || r < 0) return 0LL;
+		return fact[n] * factInv[n-r] % MOD * factInv[r] % MOD;
+	};
+    int N;
+    while(cin >> N){
+        long long res = modPow(3, N);
+        for(int i=N/2+1;i<=N;i++){
+            long long sub = comb(N, i) * modPow(2, N-i);
+            res = (res + MOD - 2 * sub % MOD) % MOD;
+        }
+        cout << res << endl;
+    }
+}
