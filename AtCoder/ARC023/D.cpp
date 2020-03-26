fe01d520c831af0641629ae5926c26b0dc15f3fd
Fumiya Suto <fumiya@soundhound.com>
2020-03-26 21:20:01 +0900

    update ARC023 D to use SegmentTree

--- AtCoder/ARC023/D.cpp
+++ AtCoder/ARC023/D.cpp
@@ -6,39 +6,71 @@ using namespace std;
 
 int gcd(int a, int b){ return a%b ? gcd(b, a%b) : b; }
 
+template<typename T>
+class SegTree {
+public:
+    explicit SegTree(int n, T def) : N(calcN_(n)), def(def), mVal(2*calcN_(n)-1, def) {}
+    void update(int idx, T value){
+        int i = N + idx - 1;
+        update_(mVal[i], value);
+        while(i > 0){
+            i = (i-1)/2;
+            mVal[i] = operate(mVal[2*i+1], mVal[2*i+2]);
+        }
+    }
+    T get(int l, int r){
+        return getImpl_(l, r, 0, 0, N);
+    }
+    int find(int l, int r, T value){
+        return findImpl_(l, r, value, 0, 0, N);
+    }
+private:
+    int calcN_(int n){
+        int res = 1;
+        while(res < n) res *= 2;
+        return res;
+    }
+    T getImpl_(int l, int r, int idx, int rangeL, int rangeR){
+        if(r <= rangeL || rangeR <= l) return def;
+        if(l <= rangeL && rangeR <= r) return mVal[idx];
+        int rangeM = (rangeL+rangeR)/2;
+        T a = getImpl_(l, r, 2*idx+1, rangeL, rangeM);
+        T b = getImpl_(l, r, 2*idx+2, rangeM, rangeR);
+        return operate(a, b);
+    }
+    T findImpl_(int l, int r, T value, int idx, int rangeL, int rangeR){
+        if(rangeR - rangeL == 1){
+            return operate(value, mVal[idx]) == value ? rangeR : rangeL;
+        }
+        int rangeM = (rangeL+rangeR)/2;
+        T a = getImpl_(l, r, 2*idx+1, rangeL, rangeM);
+        if(operate(value, a) != value) return findImpl_(l, r, value, 2*idx+1, rangeL, rangeM);
+        T b = getImpl_(l, r, 2*idx+2, rangeM, rangeR);
+        if(operate(value, b) != value) return findImpl_(l, r, value, 2*idx+2, rangeM, rangeR);
+        return r;
+    }
+    void update_(T& data, T val) { data = val; }
+    T operate(T a, T b) { return !a ? b : !b ? a : gcd(a, b); }
+    const int N;
+    const T def;
+    vector<T> mVal;
+};
+
 map<int, long long> solve(const vector<int>& a){
     const int n = a.size();
-    const int b = 256;
-    vector<int> blk;
-    for(int i=0;i<n;i+=b){
-        int g = a[i];
-        for(int j=i+1;j<min(n, i+b);j++) g = gcd(g, a[j]);
-        blk.emplace_back(g);
-    }
+    SegTree<int> seg(n, 0);
+    for(int i=0;i<n;i++) seg.update(i, a[i]);
     map<int, long long> res;
     for(int i=0;i<n;i++){
-        int src = i;
+        int idx = i;
         int cur = a[i];
-        int seg = i/b;
-        for(int j=i+1;j<n;j++){
-            if(seg != j/b){
-                int nxt = gcd(cur, blk[j/b]);
-                if(nxt == cur){
-                    j += b-1;
-                    continue;
-                }
-                seg = j/b;
-            }
-            int nxt = gcd(cur, a[j]);
-            if(cur != nxt){
-                if(!res.count(cur)) res[cur] = 0;
-                res[cur] += j-src;
-                src = j;
-                cur = nxt;
-            }
+        while(idx < n){
+            cur = gcd(cur, a[idx]);
+            int end = seg.find(idx, n, cur);
+            if(!res.count(cur)) res[cur] = 0;
+            res[cur] += end - idx;
+            idx = end;
         }
-        if(!res.count(cur)) res[cur] = 0;
-        res[cur] += n-src;
     }
     return res;
 }
