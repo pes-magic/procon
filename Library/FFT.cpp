dacdffe3542b3bea8c33e36ab4e31217b14fd310
Fumiya Suto <fumiya@soundhound.com>
2020-05-06 15:39:01 +0900

    update FFT

--- Library/FFT.cpp
+++ Library/FFT.cpp
@@ -1,28 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <complex>
-
-using namespace std;
+// FFT
+// verifyed
+// https://atcoder.jp/contests/atc001/tasks/fft_c
 
 const double PI = acos(-1.0);
 using P = complex<double>;
-
-// void dft(vector<P>& v, int inv){
-//     const int n = v.size();
-//     if(n == 1) return;
-//     vector<P> a(n/2), b(n/2);
-//     for(int i=0;i<n/2;i++){
-//         a[i] = v[2*i];
-//         b[i] = v[2*i+1];
-//     }
-//     dft(a, inv);
-//     dft(b, inv);
-//     P cur = P(1, 0), rot = P(cos(2*PI/n), inv * sin(2*PI/n));
-//     for(int i=0;i<n;i++){
-//         v[i] = a[i%(n/2)] + b[i%(n/2)] * cur;
-//         cur *= rot;
-//     }
-// }
 
 void dft(vector<P>& v, int inv){
     const int n = v.size();
@@ -60,17 +41,4 @@ vector<int> convolution(const vector<P>& a, const vector<P>& b){
     vector<int> res(n);
     for(int i=0;i<n;i++) res[i] = (ab[i].real()/n + 0.5);
     return res;
-}
-
-int main(){
-    int N; cin >> N;
-    vector<P> a, b;
-    for(int i=0;i<N;i++){
-        int x, y; cin >> x >> y;
-        a.emplace_back(x, 0);
-        b.emplace_back(y, 0);
-    }
-    auto res = convolution(a, b);
-    cout << 0 << endl;
-    for(int i=0;i<2*N-1;i++) cout << res[i] << endl;
 }
---
+++ Library/NTT.cpp
@@ -0,0 +1,57 @@
+// 整数環FFT
+// verifyed
+// https://atcoder.jp/contests/atc001/tasks/fft_c
+
+const int MOD = 998244353;
+
+long long modPow(long long a, long long p){
+    if(p == 0) return 1;
+    auto res = modPow(a, p/2);
+    res = (res*res)%MOD;
+    if(p%2) res = (res*a)%MOD;
+    return res;
+}
+
+long long calcInv(long long a){
+    return modPow(a, MOD-2);
+}
+
+void ntt(vector<long long>& v, int inv){
+    const int n = v.size();
+    auto w = modPow(3, (MOD-1)/n);
+    if(inv == -1) w = calcInv(w);
+    int rev = 0;
+    for(int i=1;i<n-1;i++){
+        for(int j=n/2;j>(rev^=j);j/=2);
+        if(i < rev) swap(v[i], v[rev]);
+    }
+    for(int m=1;m<n;m*=2){
+        auto cur = 1;
+        auto rot = modPow(w, n/(2*m));
+        for(int i=0;i<m;i++){
+            for(int j=i;j<n;j+=2*m){
+                auto p = v[j];
+                auto q = v[j+m] * cur % MOD;
+                v[j] = (p + q) % MOD;
+                v[j+m] = (p + MOD - q) % MOD;
+            }
+            cur = (cur * rot) % MOD;
+        }
+    }
+}
+
+vector<long long> convolution(const vector<long long>& a, const vector<long long>& b){
+    int _n = a.size() + b.size();
+    int n = 1;
+    while(n < _n) n *= 2;
+    vector<long long> na(n, 0), nb(n, 0);
+    for(int i=0;i<a.size();i++) na[i] = a[i];
+    for(int i=0;i<b.size();i++) nb[i] = b[i];
+    ntt(na, 1);
+    ntt(nb, 1);
+    for(int i=0;i<n;i++) na[i] = na[i] * nb[i] % MOD;
+    ntt(na, -1);
+    auto inv = calcInv(n);
+    for(int i=0;i<n;i++) na[i] = na[i] * inv % MOD;
+    return na;
+}
