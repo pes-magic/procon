d5af5284e17aaa51379182174eed7becf373ce8c
Fumiya Suto <fumiya@soundhound.com>
2020-05-25 00:08:04 +0900

    update AGC041 B, C

--- AtCoder/AGC041/B.cpp
+++ AtCoder/AGC041/B.cpp
@@ -5,32 +5,32 @@
 using namespace std;
 
 int main(){
-    long long N, M, V, P;
-    while(cin >> N >> M >> V >> P){
-        vector<long long> A(N);
-        for(auto& t : A) cin >> t;
-        sort(A.begin(), A.end());
-        int L = -1, R = N;
-        while(R - L > 1){
-            int mid = (L+R)/2;            
-            if(N - mid <= P){
-                R = mid;
-                continue;
-            }
-            long long vote = M*V;
-            bool ok = true;
-            for(int i=N-1;i>=0;i--){
-                if(i >= N-(P-1) || i == mid){
-                    vote -= M;
+    long long N, M, V, P; cin >> N >> M >> V >> P;
+    vector<long long> A(N);
+    for(auto& t : A) cin >> t;
+    sort(A.begin(), A.end());
+    int L = -1, R = N-1;
+    while(R-L > 1){
+        int mid = (L+R)/2;
+        long long thr = A[mid] + M;
+        int gr = 0;
+        long long add = M;
+        for(int j=N-1;j>=0;j--){
+            if(j == mid) continue;
+            if(A[j] > thr){
+                gr++;
+                add += M;
+            } else {
+                if(gr+1 < P && A[j]+M > thr){
+                    gr++;
+                    add += M;
                 } else {
-                    auto upper = A[mid] + M;
-                    if(upper < A[i]) ok = false;
-                    vote -= min(upper - A[i], M);
+                    add += min(M, thr-A[j]);
                 }
             }
-            if(ok && vote <= 0) R = mid;
-            else L = mid;
         }
-        cout << N-R << endl;
+        if(gr < P && add >= M*V) R = mid;
+        else L = mid;
     }
+    cout << N-R << endl;
 }
--- AtCoder/AGC041/C.cpp
+++ AtCoder/AGC041/C.cpp
@@ -4,150 +4,122 @@
 
 using namespace std;
 
-uint32_t xorshift(void) {
-    static uint32_t y = 2463534242;
-    y = y ^ (y << 13); y = y ^ (y >> 17);
-    return y = y ^ (y << 5);
-}
-
-bool valid(vector<vector<int>>& board){
+bool solve(vector<string>& board, int p, vector<int>& row, vector<int>& col, int ch){
     const int N = board.size();
-    for(int i=0;i<N;i++){
-        vector<int> use(N*N, 0);
-        int cnt = 0;
-        for(int j=0;j<N;j++){
-            if(board[i][j] == -1) continue;
-            if(use[board[i][j]]) continue;
-            use[board[i][j]] = 1;
-            ++cnt;
-        }
-        if(cnt != 3) return false;
+    if(p == N*N){
+        bool ok = true;
+        for(int i=0;i<N;i++) if(row[i] != 3 || col[i] != 3) ok = false;
+        return ok;
     }
-    for(int j=0;j<N;j++){
-        vector<int> use(N*N, 0);
-        int cnt = 0;
-        for(int i=0;i<N;i++){
-            if(board[i][j] == -1) continue;
-            if(use[board[i][j]]) continue;
-            use[board[i][j]] = 1;
-            ++cnt;
-        }
-        if(cnt != 3) return false;
+    int x = p/N, y = p%N;
+    if(y != N-1 || row[x] == 3) if(solve(board, p+1, row, col, ch)) return true;
+    if(y+1 < N && board[x][y] == '.' && board[x][y+1] == '.'){
+        board[x][y] = board[x][y+1] = 'a'+ch;
+        row[x]++;
+        col[y]++;
+        col[y+1]++;
+        if(row[x] <= 3 && col[y] <= 3 && col[y+1] <= 3) if(solve(board, p+2, row, col, ch+1)) return true;
+        board[x][y] = board[x][y+1] = '.';
+        row[x]--;
+        col[y]--;
+        col[y+1]--;
     }
-    return true;
+    if(x+1 < N && board[x][y] == '.' && board[x+1][y] == '.'){
+        board[x][y] = board[x+1][y] = 'a' + ch;
+        row[x]++;
+        row[x+1]++;
+        col[y]++;
+        if(row[x] <= 3 && row[x+1] <= 3 && col[y] <= 3) if(solve(board, p+1, row, col, ch+1)) return true;
+        board[x][y] = board[x+1][y] = '.';
+        row[x]--;
+        row[x+1]--;
+        col[y]--;
+    }
+    return false;
 }
 
-vector<vector<int>> find(int N){
-    while(true){
-        vector<vector<int>> board(N, vector<int>(N, -1));
-        for(int i=0;i<2*N;i++){
-            int err = 0;
-            while(err++ < 100){
-                int x = xorshift()%N;
-                int y = xorshift()%N;
-                int dx = xorshift()%2;
-                int dy = 1 - dx;
-                if(x + dx >= N) continue;
-                if(y + dy >= N) continue;
-                if(board[x][y] != -1) continue;
-                if(board[x+dx][y+dy] != -1) continue;
-                board[x][y] = board[x+dx][y+dy] = i;
-                break;
-            }
-        }
-        if(valid(board)){
-            return board;
-        }
-    }
+vector<string> search(int N){
+    vector<string> res(N, string(N, '.'));
+    vector<int> row(N, 0), col(N, 0);
+    solve(res, 0, row, col, 0);
+    return res;
 }
 
 int main(){
-    // for(int i=4;i<=7;i++){
-    //     auto board = find(i);
-    //     cout << "string res" << i << "[] = {" << endl;
-    //     auto chr = [](int t) -> char {
-    //         return t < 0 ? '.' : 'a' + t;
-    //     };
-    //     for(auto& v : board){
-    //         cout << "    \"";
-    //         for(auto& c : v) cout << (char)chr(c);
-    //         cout << "\"," << endl;
-    //     }
-    //     cout << "};" << endl;
-    // }
-    int N;
-    while(cin >> N){
-        if(N == 2){
-            cout << -1 << endl;
-        } else if(N == 3){
-            cout << "aab" << endl;
-            cout << "b.b" << endl;
-            cout << "baa" << endl;
-        } else {
-            vector<string> vs(N, string(N, '.'));
-            string res4[] = {
-                "gghe",
-                "ddhe",
-                "fcbb",
-                "fcaa",
-            };
-            string res5[] = {
-                "ee.bc",
-                "..abc",
-                "hhajj",
-                "dff.g",
-                "d.iig",
-            };
-            string res6[] = {
-                ".d.aae",
-                "id...e",
-                "i.kff.",
-                "j.k..g",
-                "jcc..g",
-                "bbllhh",
-            };
-            string res7[] = {
-                "llm.ee.",
-                "..mh..f",
-                ".aah..f",
-                "ii...jb",
-                "g....jb",
-                "g..kk.d",
-                "..ccnnd",
-            };
-            int s = 0;
-            if(N%4 == 1){
-                for(int i=0;i<5;i++){
-                    for(int j=0;j<5;j++){
-                        vs[i][j] = res5[i][j];
-                    }
-                }
-                s = 5;
-            }
-            if(N%4 == 2){
-                for(int i=0;i<6;i++){
-                    for(int j=0;j<6;j++){
-                        vs[i][j] = res6[i][j];
-                    }
-                }
-                s = 6;
-            }
-            if(N%4 == 3){
-                for(int i=0;i<7;i++){
-                    for(int j=0;j<7;j++){
-                        vs[i][j] = res7[i][j];
-                    }
-                }
-                s = 7;
-            }
-            for(;s<N;s+=4){
-                for(int i=0;i<4;i++){
-                    for(int j=0;j<4;j++){
-                        vs[s+i][s+j] = res4[i][j];
-                    }
-                }
-            }
-            for(auto& s : vs) cout << s << endl;
-        }
+    int N; cin >> N;
+    if(N < 3){
+        cout << -1 << endl;
+        return 0;
     }
+    if(N == 3){
+        cout << "aab" << endl;
+        cout << "b.b" << endl;
+        cout << "baa" << endl;
+        return 0;
+    }
+    vector<string> vs(N, string(N, '.'));
+    int idx = 0;
+    if(N%4==1){
+        string a5[] = {
+            "..abc",
+            "..abc",
+            "ddeef",
+            "ggh.f",
+            "iihjj"
+        };
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                vs[i][j] = a5[i][j];
+            }
+        }
+        idx = 5;
+    }
+    if(N%4==2){
+        string a6[] = {
+            "...abc",
+            "...abc",
+            "ddee.f",
+            "..gghf",
+            "iij.h.",
+            "kkj.ll"
+        };
+        for(int i=0;i<6;i++){
+            for(int j=0;j<6;j++){
+                vs[i][j] = a6[i][j];
+            }
+        }
+        idx = 6;
+    }
+    if(N%4==3){
+        string a7[] = {
+            "....abc",
+            "....abc",
+            "....def",
+            "....def",
+            "gghh..i",
+            "jjkk..i",
+            "llmmnn."
+        };
+        for(int i=0;i<7;i++){
+            for(int j=0;j<7;j++){
+                vs[i][j] = a7[i][j];
+            }
+        }
+        idx = 7;
+    }
+    while(idx < N){
+        string a4[] = {
+            "aabc",
+            "ddbc",
+            "efgg",
+            "efhh"
+        };
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                vs[idx+i][idx+j] = a4[i][j];
+            }
+        }
+        idx += 4;
+    }
+    for(auto& s : vs) cout << s << endl;
 }
