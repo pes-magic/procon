3bc325f600b4f60836aed1c02b31bcfddfaf748f
Fumiya Suto <fumiya@soundhound.com>
2020-08-22 10:48:32 +0900

    replace tab in ModuloCalc.cpp!

--- Library/ModuloCalc.cpp
+++ Library/ModuloCalc.cpp
@@ -35,19 +35,19 @@ int moduloLog(int a, int target){
 }
 
     const int maxSize = 100001;
-	vector<long long> inv(maxSize);
-	vector<long long> fact(maxSize);
-	vector<long long> factInv(maxSize);
-	for(int i=0;i<2;i++) inv[i] = fact[i] = factInv[i] = 1;
-	for(int i=2;i<maxSize;i++){
-		inv[i] = inv[MOD % i] * (MOD - MOD / i) % MOD;
-		fact[i] = fact[i-1] * i % MOD;
-		factInv[i] = factInv[i-1] * inv[i] % MOD;
-	}
-	auto comb = [&](int n, int r){
-		if(n < r || r < 0) return 0LL;
-		return fact[n] * factInv[n-r] % MOD * factInv[r] % MOD;
-	};
+    vector<long long> inv(maxSize);
+    vector<long long> fact(maxSize);
+    vector<long long> factInv(maxSize);
+    for(int i=0;i<2;i++) inv[i] = fact[i] = factInv[i] = 1;
+    for(int i=2;i<maxSize;i++){
+        inv[i] = inv[MOD % i] * (MOD - MOD / i) % MOD;
+        fact[i] = fact[i-1] * i % MOD;
+        factInv[i] = factInv[i-1] * inv[i] % MOD;
+    }
+    auto comb = [&](int n, int r){
+        if(n < r || r < 0) return 0LL;
+        return fact[n] * factInv[n-r] % MOD * factInv[r] % MOD;
+    };
     auto perm = [&](int n, int r){
         if(n < r || r < 0) return 0LL;
         return fact[n] * factInv[n-r] % MOD;
