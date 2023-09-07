bdf8fc4ef6dd6c32518882714f49be7646040e41
Fumiya Suto <fumiya.suto@gmail.com>
2023-09-07 21:35:09 +0900

    update setup_cargo for new judge

--- AtCoder/setup_cargo.py
+++ AtCoder/setup_cargo.py
@@ -1,18 +1,17 @@
-'''
+"""
 setup_cargo.py
 rust用のプロジェクトを生成する
-'''
+"""
 import argparse
 import os
 import shutil
 import subprocess
 import toml
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('contest', type=str, help='name of contest')
-    parser.add_argument('problem_id', type=str,
-                        help='number of problems')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument("contest", type=str, help="name of contest")
+    parser.add_argument("problem_id", type=str, help="number of problems")
 
     current_dir = os.getcwd()
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,35 +20,91 @@ if __name__ == '__main__':
     problems = []
     try:
         num = int(args.problem_id)
-        problems.extend(map(chr, range(ord('a'), ord('a')+num)))
+        problems.extend(map(chr, range(ord("a"), ord("a") + num)))
     except:
         problems.append(args.problem_id)
 
     try:
         os.chdir(script_dir)
-        subprocess.run(['cargo', 'init' if os.path.exists(f'./{args.contest}') else 'new', args.contest])
+        subprocess.run(["cargo", "init" if os.path.exists(f"./{args.contest}") else "new", args.contest])
 
         crates = [
-            'num', 'num-derive', 'ndarray', 'nalgebra', 'libm',
-            'rand', 'rand_distr', 'petgraph', 'indexmap', 'regex',
-            'lazy_static', 'ordered-float', 'ascii', 'permutohedron', 'superslice',
-            'itertools', 'itertools-num', 'maplit',  'either', 'im-rc',
-            'fixedbitset', 'bitset-fixed', 'proconio', 'text_io', 'whiteread',
-            'rustc-hash', 'smallvec'
+            "ac-library-rs@0.1.1",
+            "once_cell@1.18.0",
+            "static_assertions@1.1.0",
+            "varisat@0.2.2",
+            "memoise@0.3.2",
+            "argio@0.2.0",
+            "bitvec@1.0.1",
+            "counter@0.5.7",
+            "hashbag@0.1.11",
+            "pathfinding@4.3.0",
+            "recur-fn@2.2.0",
+            "indexing@0.4.1",
+            "amplify@3.14.2",
+            "amplify_derive@2.11.3",
+            "amplify_num@0.4.1",
+            "easy-ext@1.0.1",
+            "multimap@0.9.0",
+            "btreemultimap@0.1.1",
+            "bstr@1.6.0",
+            "az@1.2.1",
+            "glidesort@0.1.2",
+            "tap@1.0.1",
+            "omniswap@0.1.0",
+            "multiversion@0.7.2",
+            "num@0.4.1",
+            "num-bigint@0.4.3",
+            "num-complex@0.4.3",
+            "num-integer@0.1.45",
+            "num-iter@0.1.43",
+            "num-rational@0.4.1",
+            "num-traits@0.2.15",
+            "num-derive@0.4.0",
+            "ndarray@0.15.6",
+            "nalgebra@0.32.3",
+            "alga@0.9.3",
+            "libm@0.2.7",
+            "rand@0.8.5",
+            "getrandom@0.2.10",
+            "rand_chacha@0.3.1",
+            "rand_core@0.6.4",
+            "rand_hc@0.3.2",
+            "rand_pcg@0.3.1",
+            "rand_distr@0.4.3",
+            "petgraph@0.6.3",
+            "indexmap@2.0.0",
+            "regex@1.9.1",
+            "lazy_static@1.4.0",
+            "ordered-float@3.7.0",
+            "ascii@1.1.0",
+            "permutohedron@0.2.4",
+            "superslice@1.0.0",
+            "itertools@0.11.0",
+            "itertools-num@0.1.3",
+            "maplit@1.0.2",
+            "either@1.8.1",
+            "im-rc@15.1.0",
+            "fixedbitset@0.4.2",
+            "bitset-fixed@0.1.0",
+            "proconio@0.4.5",
+            "text_io@0.1.12",
+            "rustc-hash@1.1.0",
+            "smallvec@1.11.0",
         ]
 
         os.chdir(os.path.join(script_dir, args.contest))
-        subprocess.run(['cargo', 'add'] + crates)
+        subprocess.run(["cargo", "add"] + crates)
 
-        t = toml.load(open('./Cargo.toml'))
-        t['package']['name'] = t['package']['name'].lower()
-        toml.dump(t, open('./Cargo.toml', 'w'))
+        t = toml.load(open("./Cargo.toml"))
+        t["package"]["name"] = t["package"]["name"].lower()
+        toml.dump(t, open("./Cargo.toml", "w"))
 
-        os.makedirs('src/bin', exist_ok=True)
+        os.makedirs("src/bin", exist_ok=True)
 
         for id in problems:
-            if not os.path.exists(f'src/bin/{id}.rs'):
-                shutil.copy('src/main.rs', f'src/bin/{id}.rs')
-        os.remove('src/main.rs')
+            if not os.path.exists(f"src/bin/{id}.rs"):
+                shutil.copy("src/main.rs", f"src/bin/{id}.rs")
+        os.remove("src/main.rs")
     finally:
         os.chdir(current_dir)
